93ba6569520b5985b99a6678f91dd707f0f7f147
Cole Carrow <colec518@gmail.com>
2017-02-06 20:45:34 -0500

    cole@DESKTOP-CP4BMK9:~$

--- Roborio/src/org/usfirst/frc/team6325/robot/subsystems/GearMechanism.java
+++ Roborio/src/org/usfirst/frc/team6325/robot/subsystems/GearMechanism.java
@@ -1,1 +1,31 @@
+package org.usfirst.frc.team6325.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class GearMechanism extends Subsystem {
+
+	Compressor compressor = new Compressor(0);
+	
+	DoubleSolenoid piston = new DoubleSolenoid(1, 2);
+	
+	public void init(){
+		compressor.setClosedLoopControl(true);
+		close();
+	}
+	
+	public void open(){
+		piston.set(DoubleSolenoid.Value.kForward);
+	}
+	
+	public void close(){
+		piston.set(DoubleSolenoid.Value.kReverse);
+	}
+	
+	public void initDefaultCommand() {
+
+		// setDefaultCommand(new MySpecialCommand());
+	}
+}
 
