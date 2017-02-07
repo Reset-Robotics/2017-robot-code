131cab2ea00eb3eb2747b88e069caed7c33c0a48
Cyco12 <PGD.daigle@gmail.com>
2017-02-07 10:16:37 -0500

    Deleted Vulkan Library
    Library was not necessary

--- Vision/VulkanSDK/setup-env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export VULKAN_SDK=$PWD/x86_64
-export PATH=$VULKAN_SDK/bin:$PATH
-export LD_LIBRARY_PATH=$VULKAN_SDK/lib:$LD_LIBRARY_PATH
-export VK_LAYER_PATH=$VULKAN_SDK/etc/explicit_layer.d
-
