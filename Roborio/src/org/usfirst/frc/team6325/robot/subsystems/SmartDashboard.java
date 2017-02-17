e878b76989bf5efdbe8ef27f10c3ff5f87199257
leejasper851 <leejasper851@gmail.com>
2017-02-16 19:54:43 -0500

    Create SmartDashboard.java

---
+++ Roborio/src/org/usfirst/frc/team6325/robot/subsystems/SmartDashboard.java
@@ -0,0 +1,34 @@
+package org.usfirst.frc.team6325.robot.subsystems;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class SmartDashboard extends Subsystem {
+
+        public void frontLeftSpeed() {
+                SmartDashboard.putNumber("Front Left Motor Speed", MecanumDrive.getFrontLeftSpeed());
+        }
+        public void frontRightSpeed() {
+                SmartDashboard.putNumber("Front Right Motor Speed", MecanumDrive.getFrontRightSpeed());
+        }
+        public void backLeftSpeed() {
+                SmartDashboard.putNumber("Back Left Motor Speed", MecanumDrive.getBackLeftSpeed());
+        }
+        public void backRightSpeed() {
+                SmartDashboard.putNumber("Back Right Motor Speed", MecanumDrive.getBackRightSpeed());
+        }
+        
+        public void mecanumDriveCommands() {
+                SmartDashboard.putData(MecanumDrive);
+        }
+        public void gearMechanismCommands() {
+                SmartDashboard.putData(GearMechanism);
+        }
+        public void shooterCommands() {
+                SmartDashboard.putData(Shooter);
+        }
+        public void climberCommands() {
+                SmartDashboard.putData(Climber);
+        }
+        
+        public void initDefaultCommand() {
+        }
+}
