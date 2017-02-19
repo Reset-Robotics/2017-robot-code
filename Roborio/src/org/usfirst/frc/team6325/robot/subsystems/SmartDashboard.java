463d0d98b8fff568304e4ae306fdcff65156d636
leejasper851 <leejasper851@gmail.com>
2017-02-19 11:18:06 -0500

    Update SmartDashboard.java

--- Roborio/src/org/usfirst/frc/team6325/robot/subsystems/SmartDashboard.java
+++ Roborio/src/org/usfirst/frc/team6325/robot/subsystems/SmartDashboard.java
@@ -3,39 +3,19 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class SmartDashboard extends Subsystem {
 
-        public void frontLeftSpeed() {
+        public void displaySmartDashboard() {
                 SmartDashboard.putNumber("Front Left Motor Speed", MecanumDrive.getFrontLeftSpeed());
-        }
-        public void frontRightSpeed() {
                 SmartDashboard.putNumber("Front Right Motor Speed", MecanumDrive.getFrontRightSpeed());
-        }
-        public void backLeftSpeed() {
                 SmartDashboard.putNumber("Back Left Motor Speed", MecanumDrive.getBackLeftSpeed());
-        }
-        public void backRightSpeed() {
                 SmartDashboard.putNumber("Back Right Motor Speed", MecanumDrive.getBackRightSpeed());
-        }
-        
-        public void mecanumDriveCommands() {
+
                 SmartDashboard.putData(MecanumDrive);
-        }
-        public void gearMechanismCommands() {
                 SmartDashboard.putData(GearMechanism);
-        }
-        public void shooterCommands() {
                 SmartDashboard.putData(Shooter);
-        }
-        public void climberCommands() {
                 SmartDashboard.putData(Climber);
-        }
-        
-        public void shooterAngle() {
+
                 SmartDashboard.putNumber("Shooter Angle", Shooter.getAngle());
-        }
-        public void pistonPosition() {
                 SmartDashboard.putNumber("Gear Piston Position", GearMechanism.getValue());
-        }
-        
         public void initDefaultCommand() {
         }
 }
