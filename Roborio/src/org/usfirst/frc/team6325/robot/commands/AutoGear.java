f5222ce2053e0611320830cbb660875ad0e15c16
prajy <prajwalvepicx@gmail.com>
2017-02-21 23:31:24 -0500

    Update AutoGear.java

--- Roborio/src/org/usfirst/frc/team6325/robot/commands/AutoGear.java
+++ Roborio/src/org/usfirst/frc/team6325/robot/commands/AutoGear.java
@@ -7,26 +7,50 @@ import org.usfirst.frc.team6325.robot.Robot;
 /**
  *
  */
-public class ExampleCommand extends Command {
-	public ExampleCommand() {
+
+// distance = targetHeightinInches (5 inches) * imageWidth(640) / (2*targetHeightPixels*tan(cameraHorizontalAngle/2)
+public class AutoGear extends Command {
+	boolean done = false;
+	
+	public AutoGear() {
 		// Use requires() here to declare subsystem dependencies
-		requires(Robot.exampleSubsystem);
+		requires(Robot.mecanumDrive);
+		requires(Robot.GearMechanism);
 	}
 
 	// Called just before this Command runs the first time
 	@Override
-	protected void initialize() {
+	protected void initialize() 
+	{
+		
 	}
 
 	// Called repeatedly when this Command is scheduled to run
 	@Override
-	protected void execute() {
+	protected void execute() 
+	{
+		while(currentDistanceToPeg > neededDistanceToPeg)
+		{
+			Robot.mecanumDrive.cartesianDrive(0, 1, 0, 0.5);
+		}
+		//Should probably add code that lines up robot on X axis
+		Robot.GearMechanism.open();
+		Timer.delay(1);
+		Robot.mecanumDrive.cartesianDrive(-0.25, 0, 0, 0.5);
+		Timer.delay(0.25);
+		Robot.mecanumDrive(cartesianDrive(0, -1, 0, 0.5);
+		Timer.delay(2);
+		Robot.GearMechanism.close();
+		done = true;
+				 
+				   
+			
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
 	@Override
 	protected boolean isFinished() {
-		return false;
+		return done;
 	}
 
 	// Called once after isFinished returns true
