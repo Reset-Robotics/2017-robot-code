683fd4df6e0e40f57886ea84dee4ed6b58b97c91
prajy <prajwalvepicx@gmail.com>
2017-03-15 10:50:50 -0400

    updates

--- Vision/main.cpp
+++ Vision/main.cpp
@@ -3,7 +3,7 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include "opencv2/gpu/gpu.hpp"
 
-//standard package imports
+//standard imports
 #include <iostream>
 #include <time.h>
 #include <cstdio>
@@ -11,27 +11,31 @@
 #include <cstdlib>
 
 int main() {
+	
+	printf("Current exposure settings:\n");
+        system("v4l2-ctl -d /dev/video2 -C exposure_auto");
+        system("v4l2-ctl -d /dev/video2 -C exposure_absolute");
+
+        //set exposure to 5
+        system("v4l2-ctl -d /dev/video2 -c exposure_auto=1");
+        system("v4l2-ctl -d /dev/video2 -c exposure_absolute=5");
+
+        //print the exposures of the cameras
+        printf("New exposure settings:\n");
+        system("v4l2-ctl -d /dev/video2 -C exposure_absolute");
+        
+
 	int blur_size = 3; //size of the median blur kernel
-	int img_scale_factor = 2; //halves the size of the picture
+	// int img_scale_factor = 2; //halves the size of the picture
 
-	//position tracking values (piecewise function)
-	double angle_approx[6] = {-18.3069, 113.2350, -253.0048, 228.7990, 4.0635, 15.4875}; //ar^5 + br^4 + cr^3 + dr^2 + er + f (for 0.3 <= r <= 1.8)
-	double angle_approx_low[2] = {105.2632, 0.0}; //ar + b (for r < 0.3)
-	double angle_approx_high[2] = {3.9761, 127.1074}; //ar + b (for r > 1.8)
-
-	//average of past three frame's values for less fluctuation on angle
-	double area_ratio_vals[3] = {1.0, 1.0, 1.0};
-
-	//distance tracking values
-	double dist_approx[4] = {4.7720, 2.7183, -0.0381, 0.3698}; //a(b^cr) + d
 
 	//HSV Thresholding Defaults (green LED ring and Exposure of 5 on camera)
-	int h_lowerb = 20;
-	int h_upperb = 90;
-	int s_lowerb = 130;
-	int s_upperb = 255;
-	int v_lowerb = 25;
-	int v_upperb = 130;
+	int h_low = 0;
+	int h_up = 92;
+	int s_low = 0;
+	int s_up = 10;
+	int v_low = 0;
+	int v_up = 255;
 
 	//images
 	cv::Mat imgRaw; //input image
@@ -44,21 +48,21 @@ int main() {
 
 	cv::gpu::GpuMat src, resize, blur, hsv, threshold;
 
-	//HSV Threshold Sliders (currently commented out cause unnecessary)
+	//HSV Threshold Sliders
 	cv::namedWindow("HSV Thresholding");
-	cv::createTrackbar("Hue Lower Bound", "HSV Thresholding", &h_lowerb, 179);
-	cv::createTrackbar("Hue Upper Bound", "HSV Thresholding", &h_upperb, 179);
-	cv::createTrackbar("Saturation Lower Bound", "HSV Thresholding", &s_lowerb, 255);
-	cv::createTrackbar("Saturation Upper Bound", "HSV Thresholding", &s_upperb, 255);
-	cv::createTrackbar("Value Lower Bound", "HSV Thresholding", &v_lowerb, 255);
-	cv::createTrackbar("Value Upper Bound", "HSV Thresholding", &v_upperb, 255);
+	cv::createTrackbar("Hue Lower Bound", "HSV Thresholding", &h_low, 179);
+	cv::createTrackbar("Hue Upper Bound", "HSV Thresholding", &h_up, 179);
+	cv::createTrackbar("Saturation Lower Bound", "HSV Thresholding", &s_low, 255);
+	cv::createTrackbar("Saturation Upper Bound", "HSV Thresholding", &s_up, 255);
+	cv::createTrackbar("Value Lower Bound", "HSV Thresholding", &v_low, 255);
+	cv::createTrackbar("Value Upper Bound", "HSV Thresholding", &v_up, 255);
 
-	std::vector<std::vector<cv::Point> > contours; //array of contours (which are each an array of points)
+	std::vector<std::vector<cv::Point> > contours; //array of contour points
 
 
 
 	//start the video
-	cv::VideoCapture input(0);
+	cv::VideoCapture input(2);
 	cv::VideoWriter output;
 
 	//loop for each frame
@@ -73,12 +77,12 @@ int main() {
 		src.upload(imgRaw);
 
 		//gpu accelerated image transformations
-		cv::gpu::resize(src, resize, cv::Size(input.get(CV_CAP_PROP_FRAME_WIDTH) / img_scale_factor,
+		/* cv::gpu::resize(src, resize, cv::Size(input.get(CV_CAP_PROP_FRAME_WIDTH) / img_scale_factor,
 			input.get(CV_CAP_PROP_FRAME_HEIGHT) / img_scale_factor), CV_INTER_CUBIC);
-		resize.download(imgResize);
+		resize.download(imgResize); */
 		//there is no gpu accelerated median blur :(
 		//cv::medianBlur(resize, blur, blur_size);
-		cv::gpu::cvtColor(resize, hsv, CV_BGR2HSV);
+		cv::gpu::cvtColor(src, hsv, CV_BGR2HSV);
 
 		//download GpuMat from the GPU
 		hsv.download(imgHSV);
@@ -86,7 +90,7 @@ int main() {
 	//end gpu accel
 
 		//I think inrange and contours can be accelerated, so we're not done here
-		cv::inRange(imgHSV, cv::Scalar(h_lowerb, s_lowerb, v_lowerb), cv::Scalar(h_upperb, s_upperb, v_upperb), imgThreshold);
+		cv::inRange(imgHSV, cv::Scalar(h_low, s_low, v_low), cv::Scalar(h_up, s_up, v_up), imgThreshold);
 		imgThreshold.copyTo(imgContour);
 		cv::findContours(imgContour, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
 
@@ -117,20 +121,20 @@ int main() {
 
 		cv::drawContours(imgOutput, contours, -1, cv::Scalar(0, 255, 0)); //draw all contours in green
 
-		//if we identify targets print all this
+		//if we identify targets calculate centerX
 		if (!targets.empty()) {
-			std::vector<cv::RotatedRect> r(targets[0]); // try regular parentheses
-			std::vector<cv::RotatedRect> r1(targets[1]);
-			centerX= r.x + (r.width/2);
+			cv::Rect r = boundingRect(targets[0])
+			cv::Rect r1 = boundingRect(targets[1])
+			centerX= r.x + (r.width/2); // calculate the centerX of each
 			centerX1= r1.x + (r1.width/2);
 			double distanceFromCenterPixels= ((centerX + centerX1) / 2) - (640 / 2); // 640 is camera width
-			// cout this
+			
 
 		}
 
 		//save and show the output image
 		output.write(imgOutput);
-		cv::imshow("stream", imgThreshold);
+		cv::imshow("Threshold image", imgThreshold);
 
 		//if spacebar is pressed, quit
 		char c = cv::waitKey(1);
