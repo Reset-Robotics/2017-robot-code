0734af2535285de7892299e5eab223c78ce62fba
prajy <prajwalvepicx@gmail.com>
2017-04-06 00:04:35 -0400

    made file executable

--- Vision/ahoy/startup.sh
+++ Vision/ahoy/startup.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-cd build
+cd build // If script located in ahoy folder. Can also put anywhere and cd into folder or place script in build.
 ./ahoy22
 
-
 // run this after
-
+//chmod +x startup.sh
 //#crontab -e
 //@reboot  /home/user/startup.sh
